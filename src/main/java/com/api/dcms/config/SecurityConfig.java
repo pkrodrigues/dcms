cf2f0c0f5464f858297bf065a99c5513f203f7a5
pkrodrigues <pkjrodrigues@gmail.com>
2022-08-03 18:14:33 -0300

    feat: Add Swagger

---
+++ src/main/java/com/api/dcms/config/SecurityConfig.java
@@ -0,0 +1,85 @@
+package com.api.dcms.config;
+
+import com.api.dcms.security.JwtAuthFilter;
+import com.api.dcms.security.JwtService;
+import com.api.dcms.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OncePerRequestFilter jwtFilter(){
+        return new JwtAuthFilter(jwtService, usuarioService);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(usuarioService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/agenda/**")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers("/api/v1/consultas/**")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers("/api/v1/convenio/**")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers("/api/v1/paciente")
+                .hasRole("ADMIN")
+                .antMatchers("/api/v1/medico/**")
+                .hasRole("ADMIN")
+                .antMatchers("/api/v1/secretario/**")
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/v1/usuario/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+        ;
+    }
+
+  /*  @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+    }*/
+}
+
